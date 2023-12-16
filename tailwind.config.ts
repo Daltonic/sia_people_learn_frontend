6c9429c5c8c90561116cb6315239687f47451ddb
Daltonic <darlingtongospel@gmail.com>
2023-12-16 14:24:12 +0100

    modifies the tailwind config script

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,10 @@
 import type { Config } from 'tailwindcss'
-const {nextui} = require("@nextui-org/react");
 
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -15,10 +13,8 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-      
     },
   },
-  darkMode: "class",
-  plugins: [nextui()],
+  plugins: [],
 }
 export default config
