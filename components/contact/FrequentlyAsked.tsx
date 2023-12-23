d1e979be86d24671bbb65e8bea0ebaf0dcfd7ba5
Code Queen <princessuhiene@gmail.com>
2023-12-23 13:01:53 +0000

    i completed the contact mobile view

--- components/contact/FrequentlyAsked.tsx
+++ components/contact/FrequentlyAsked.tsx
@@ -15,36 +15,36 @@ const FrequentlyAsked: React.FC<{ items: AccordionItem[] }> = ({ items }) => {
   };
 
   return (
-    <div className="bg-[#F7F8FB] p-20 mt-10">
+    <div className="bg-[#F7F8FB] px-5 py-10 md:p-20 mt-10">
       <div className="text-center mb-10">
-        <h1 className="text-3xl md:text-4xl font-bold">
+        <h1 className="text-3xl md:text-4xl font-bold text-[#321463]">
           Frequently Asked Questions.
         </h1>
         <p className="text-md mt-3 capitalize text-[#4F547B]">
           Ut enim ad minim veniam, quis nostrud exercitation ullamco
         </p>
       </div>
-      <div className="space-y-3 flex flex-col items-center p-5">
+      <div className="space-y-3 flex flex-col items-center md:p-5">
         {accordion.map((item, index) => (
-          <div key={index} className="rounded-md bg-white w-3/4 p-4">
-            <div className="flex center-end h-fit gap-10">
+          <button  onClick={() => handleClick(index)} key={index} className="rounded-md bg-white md:w-3/4 p-4">
+            <div className="flex items-start h-fit gap-5 md:gap-10 text-start">
               <div>
               <button
-                onClick={() => handleClick(index)}
-                className="w-10 p-1 h-10 rounded-full flex justify-center items-center text-[#C5165D] bg-[#E5F0FD]"
+               
+               className={`w-10 p-1 h-10 rounded-full flex justify-center items-center ${openIndex === index ? 'text-white bg-[#C5165D]' : 'text-[#C5165D] bg-[#E5F0FD]'} transform hover:rotate-180 transition-all duration-700`}
               >
                 {openIndex === index ? <FaMinus /> : <FaPlus />}
               </button>
               
               </div>
               <div>
-                <p className="text-violet-950 font-medium"> {item.title}</p>
+                <p className="text-violet-950 font-medium text-lg md:text-md"> {item.title}</p>
                 <div className="mt-4 text-[#4F547B] text-sm">
               {openIndex === index && <p>{item.content}</p>}
             </div>
               </div>
             </div>
-          </div>
+          </button>
         ))}
       </div>
     </div>
--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -3,15 +3,16 @@ import FrequentlyAsked from "@/components/contact/FrequentlyAsked";
 import Hero from "@/components/contact/Hero";
 import Offices from "@/components/contact/Offices";
 import Layout from "@/components/layout/Layout";
+import { accordion } from "@/data/accordion";
 import { NextPage } from "next";
 
 const Page: NextPage = () => {
   return (
     <Layout>
-      <div className="pb-24">
+      <div className="md:pb-24">
       <Hero/>
       <Offices/>
-      <FrequentlyAsked/>
+      <FrequentlyAsked items={accordion}/>
       </div>
     </Layout>
   );
