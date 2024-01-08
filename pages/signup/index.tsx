0c6ba1d57343cc082c5cab870b3a39967292a9a3
Emmanuel Osademe <emma.osademe@gmail.com>
2024-01-08 13:35:12 +0100

    integrate endpoints for account registration and verification

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -4,15 +4,86 @@ import Button from "@/components/reusableComponents/Button";
 import InputField from "@/components/reusableComponents/InputField";
 import AuthLayout from "@/components/layout/authLayout/AuthenticationLayout";
 import Link from "next/link";
+import { ChangeEvent, SyntheticEvent, useState } from "react";
 
 const LoginPage: NextPage = () => {
+  const [signupDetails, setSignupDetails] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [submitting, setSubmitting] = useState<boolean>(false);
+
+  // todo: a function to validate that password and confirm password are same
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.currentTarget;
+
+    setSignupDetails((prevDetails) => ({
+      ...prevDetails,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    setSubmitting(true);
+
+    const { firstname, lastname, email, password, confirmPassword } =
+      signupDetails;
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+    }
+
+    const userDetails = {
+      firstName: firstname,
+      lastName: lastname,
+      email,
+      password,
+    };
+
+    try {
+      const requestDetails = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(userDetails),
+      };
+
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_URI}/api/v1/users/register`,
+        requestDetails
+      );
+
+      if (response.status === 400) {
+        alert("Something went wrong");
+      }
+
+      const { msg } = await response.json();
+      alert(msg);
+    } catch (e: any) {
+      alert(e.message);
+    } finally {
+      setSubmitting(false);
+      setSignupDetails({
+        firstname: "",
+        lastname: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      });
+    }
+  };
+
   return (
     <AuthLayout>
       <div className="flex justify-center items-center w-full h-full md:mt-16">
         <form
-          action=""
           className="bg-white p-5 md:p-10 w-full md:w-4/5 rounded-md "
-          // onSubmit={handleSubmit}
+          onSubmit={handleSubmit}
         >
           <h3 className="font-medium text-3xl text-[#321463]">Sign Up</h3>
           <p className="md:text-sm text-[#4F547B] mt-1">
@@ -23,11 +94,22 @@ const LoginPage: NextPage = () => {
           </p>
           <div className="md:flex gap-2">
             <InputField
-              label="Fullname"
-              name="fullname"
+              label="Firstname"
+              name="firstname"
               placeholder="John Doe"
               required
               inputType="text"
+              handleChange={handleChange}
+              value={signupDetails.firstname}
+            />
+            <InputField
+              label="Lastname"
+              name="lastname"
+              placeholder="John Doe"
+              required
+              inputType="text"
+              handleChange={handleChange}
+              value={signupDetails.lastname}
             />
             <InputField
               label="Email"
@@ -35,6 +117,8 @@ const LoginPage: NextPage = () => {
               placeholder="youremail@domain.com"
               required
               inputType="email"
+              handleChange={handleChange}
+              value={signupDetails.email}
             />
           </div>
 
@@ -44,15 +128,19 @@ const LoginPage: NextPage = () => {
               name="password"
               placeholder="********"
               required
-              inputType="number"
+              inputType="text"
+              handleChange={handleChange}
+              value={signupDetails.password}
             />
 
             <InputField
               label="Confirm Password"
-              name="confirmpassword"
+              name="confirmPassword"
               placeholder="********"
               required
-              inputType="number"
+              inputType="text"
+              handleChange={handleChange}
+              value={signupDetails.confirmPassword}
             />
           </div>
 
@@ -73,8 +161,16 @@ const LoginPage: NextPage = () => {
             variant="pink"
             className=""
             style={{ width: "100%", marginTop: "14px", padding: "16px" }}
+            disabled={
+              signupDetails.firstname === "" ||
+              signupDetails.lastname === "" ||
+              signupDetails.email === "" ||
+              signupDetails.password === "" ||
+              signupDetails.confirmPassword === "" ||
+              submitting
+            }
           >
-            Register
+            {submitting ? "Registering" : "Register"}
           </Button>
         </form>
       </div>
