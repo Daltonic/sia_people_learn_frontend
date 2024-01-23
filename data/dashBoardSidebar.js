a24741eb4e63a169bf21eb978ccc04debcc070de
Emmanuel Osademe <emma.osademe@gmail.com>
2024-01-23 23:57:47 +0100

    handle logout and dashboard authentication

--- data/dashBoardSidebar.js
+++ data/dashBoardSidebar.js
@@ -7,7 +7,6 @@ import SettingsSVG from "@/components/dashboard/dashboardSVGs/SettingsSVG";
 import { FaRegUser } from "react-icons/fa";
 import { LuShoppingBag } from "react-icons/lu";
 
-
 export const sidebarItems = [
   {
     id: 1,
@@ -60,6 +59,99 @@ export const sidebarItems = [
   },
   {
     id: 9,
+    href: "/(dashboard)/settings",
+    iconClass: <SettingsSVG />,
+    text: "Settings",
+  },
+];
+
+export const userSidebarItems = [
+  {
+    id: 1,
+    href: "/(dashboard)/myCourses",
+    iconClass: <PlayButtonSVG />,
+    text: "My Courses",
+  },
+  {
+    id: 2,
+    href: "/(dashboard)/wishlist",
+    iconClass: <BookmarkSVG />,
+    text: "WishLists",
+  },
+  {
+    id: 3,
+    href: "/(dashboard)/settings",
+    iconClass: <SettingsSVG />,
+    text: "Settings",
+  },
+];
+
+export const adminSidebarItems = [
+  {
+    id: 1,
+    href: "/(dashboard)/dashboard",
+    iconClass: <DiscoverySVG />,
+    text: "Dashboard",
+  },
+  {
+    id: 2,
+    href: "/(dashboard)/users",
+    iconClass: <FaRegUser />,
+    text: "Users",
+  },
+  {
+    id: 3,
+    href: "/(dashboard)/products",
+    iconClass: <LuShoppingBag />,
+    text: "Products",
+  },
+  {
+    id: 4,
+    href: "/(dashboard)/blogs",
+    iconClass: <MessagesSVG />,
+    text: "Blogs",
+  },
+  {
+    id: 5,
+    href: "/(dashboard)/settings",
+    iconClass: <SettingsSVG />,
+    text: "Settings",
+  },
+];
+
+export const instructorSidebarItems = [
+  {
+    id: 1,
+    href: "/(dashboard)/myProducts",
+    iconClass: <LuShoppingBag />,
+    text: "My Products",
+  },
+  {
+    id: 2,
+    href: "/(dashboard)/createProduct",
+    iconClass: <ListSVG />,
+    text: "Create Product",
+  },
+  {
+    id: 3,
+    href: "/(dashboard)/myCourses",
+    iconClass: <PlayButtonSVG />,
+    text: "My Courses",
+  },
+  {
+    id: 4,
+    href: "/(dashboard)/reviews",
+    iconClass: <MessagesSVG />,
+    text: "Reviews",
+  },
+  {
+    id: 5,
+    href: "/(dashboard)/wishlist",
+    iconClass: <BookmarkSVG />,
+    text: "WishLists",
+  },
+  {
+    id: 6,
     href: "/(dashboard)/settings",
     iconClass: <SettingsSVG />,
     text: "Settings",
