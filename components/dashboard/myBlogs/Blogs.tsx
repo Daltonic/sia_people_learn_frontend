7f637e967a223caff692de2af1c78d31585a6dad
Code Queen <princessuhiene@gmail.com>
2024-01-27 21:06:01 +0000

    i made a few adjustments to the myblog page

--- components/dashboard/myBlogs/Blogs.tsx
+++ components/dashboard/myBlogs/Blogs.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import BlogCard from "@/components/blogs/BlogCard";
+import Button from "@/components/reusableComponents/Button";
 import { _useContext } from "@/context/Context";
 import { IPost, IPosts } from "@/utils/type.dt";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { GoArrowUpRight } from "react-icons/go";
 
 const Blogs: React.FC = () => {
   const router = useRouter();
@@ -53,33 +55,38 @@ const Blogs: React.FC = () => {
 
   return (
     <div className="">
-      <div className="mb-10 md:mb-16  px-5 sm:px-0">
-        <h1 className="font-bold text-[#321463] text-3xl">Blogs</h1>
-        <div className="w-full flex items-center justify-between">
-          <div className="flex justify-start items-center gap-1">
-            <p className="text-[#4F547B] text-lg">My Blogs</p>
+      <div className="mb-10 md:mb-16 px-5 sm:px-0">
+        <div className="w-full flex items-start justify-between">
+          <div>
+            <h1 className="font-bold text-[#321463] text-3xl">Blogs</h1>
+            <p className="text-[#4F547B] text-lg">
+              Manage your blog entries efficiently on this page. Create, edit,
+              and publish with ease.
+            </p>
+          </div>
+          <div className="">
             <Link
-              href={`/blogs`}
-              className="rounded-lg bg-blue-300 text-white sm:mx-10 md:mx-20 px-4 py-2 max-w-[200px] cursor-pointer"
+              href="/blogs"
+              className="font-medium text-sm text-center pr-4 flex items-center rounded-md bg-[#6440FB12] text-[#1A064F]  hover:text-[#C5165D] border-2 border-transparent hover:border-[#C5165D] hover:bg-transparent w-fit"
             >
-              All Blogs
+              <Button className=""> All Blogs </Button>
+              <GoArrowUpRight className="md:-ml-4 text-lg font-bold" />
             </Link>
           </div>
-
-          <Link
-            href="/blogs/create"
-            className="rounded-lg bg-blue-500 text-white sm:mx-10 md:mx-20 px-4 py-2 max-w-[200px]"
-          >
-            Create New Blog
-          </Link>
         </div>
       </div>
-      <div className="flex justify-between  w-full flex-wrap">
+      <div className="flex justify-between w-full flex-wrap">
         {posts &&
           posts.map((post, index) => (
             <BlogCard key={post._id} blog={post} i={index} />
           ))}
       </div>
+      <Link
+        href="/blogs/create"
+        className="rounded-lg bg-[#C5165D] text-white px-4 py-2 font-medium text-sm "
+      >
+        Create New Blog
+      </Link>
     </div>
   );
 };
