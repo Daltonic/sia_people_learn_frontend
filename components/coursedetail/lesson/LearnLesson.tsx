90fb371f477e18203f05e8e3f00a6ee7d18a344b
Emmanuel Osademe <emma.osademe@gmail.com>
2024-02-20 22:26:04 +0100

    Play lesson video

--- components/coursedetail/lesson/LearnLesson.tsx
+++ components/coursedetail/lesson/LearnLesson.tsx
@@ -1,12 +1,52 @@
-import { ILesson } from "@/utils/type.dt";
+import LessonAccordion from "@/components/lesson/LessonAccordion";
+import { ICourse, ILesson } from "@/utils/type.dt";
+import ReactPlayer from "react-player";
 
 interface Props {
   lesson: ILesson;
+  course: ICourse;
 }
 
-const LearnLesson: React.FC<Props> = ({ lesson }) => {
-  console.log(lesson);
-  return <div>{lesson.description}</div>;
+const LearnLesson: React.FC<Props> = ({ lesson, course }) => {
+  //todo: Function to calculate video width and height based on screen side
+  return (
+    <div className="flex md-flex-col w-full">
+      <div className="flex flex-col  w-3/4">
+        <h1 className="text-[36px]">{lesson.title}</h1>
+        <div className="w-full flex justify-center items-center rounded-lg">
+          <ReactPlayer
+            controls
+            url={
+              lesson.videoUrl || `https://www.youtube.com/watch?v=7sDY4m8KNLc`
+            }
+            width={"640px"}
+            height={"480px"}
+            loop={true}
+            style={{
+              marginTop: "4px",
+              marginBottom: "4px",
+              width: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              padding: "4px",
+            }}
+          />
+        </div>
+
+        <div>{lesson.overview}</div>
+        <div
+          dangerouslySetInnerHTML={{ __html: lesson.description }}
+          className={"line-clamp-3 text-[#4F547B] "}
+        />
+      </div>
+      <div className="flex flex-col w-1/4">
+        <p>Course Details</p>
+        <LessonAccordion course={course} lessonId={lesson._id} />
+        <p>{course.overview}</p>
+      </div>
+    </div>
+  );
 };
 
 export default LearnLesson;
--- pages/course/learn/lesson/[id]/index.tsx
+++ pages/course/learn/lesson/[id]/index.tsx
@@ -1,14 +1,17 @@
 import Layout from "@/components/layout/Layout";
 import { GetServerSidePropsContext, NextPage } from "next";
-import { ILesson } from "@/utils/type.dt";
-import { fetchLesson } from "@/services/backend.services";
+import { ICourse, ILesson } from "@/utils/type.dt";
+import { fetchCourse, fetchLesson } from "@/services/backend.services";
 import LearnLesson from "@/components/coursedetail/lesson/LearnLesson";
 
-const Page: NextPage<{ lessonData: ILesson }> = ({ lessonData }) => {
+const Page: NextPage<{ lessonData: ILesson; courseData: ICourse }> = ({
+  lessonData,
+  courseData,
+}) => {
   return (
     <Layout>
       <div className="md:px-14 md:py-10 p-5 sm:px-10 md:relative overflow-x-hidden">
-        <LearnLesson lesson={lessonData} />
+        <LearnLesson lesson={lessonData} course={courseData} />
       </div>
     </Layout>
   );
@@ -19,15 +22,17 @@ export default Page;
 export const getServerSideProps = async (
   context: GetServerSidePropsContext
 ) => {
-  const { id } = context.query;
+  const { id, courseId } = context.query;
   const token = context.req.cookies.accessToken;
 
   try {
     const lesson = await fetchLesson(id as string, token);
+    const course = await fetchCourse(courseId as string, token);
 
     return {
       props: {
         lessonData: JSON.parse(JSON.stringify(lesson)) as ILesson,
+        courseData: JSON.parse(JSON.stringify(course)) as ICourse,
       },
     };
   } catch (e: any) {
@@ -35,6 +40,7 @@ export const getServerSideProps = async (
     return {
       props: {
         lessonData: {} as ILesson,
+        courseData: {} as ICourse,
       },
     };
   }
